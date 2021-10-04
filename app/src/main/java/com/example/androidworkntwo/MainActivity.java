656fd2310bf388f647ef8df35b94465f44a87355
lekoal <coreglus@gmail.com>
2021-10-04 19:45:56 +0300

    Edited code

--- app/src/main/java/com/example/androidworkntwo/MainActivity.java
+++ app/src/main/java/com/example/androidworkntwo/MainActivity.java
@@ -14,45 +14,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button key_1 = findViewById(R.id.key_1);
-        Button key_2 = findViewById(R.id.key_2);
-        Button key_3 = findViewById(R.id.key_3);
-        Button key_4 = findViewById(R.id.key_4);
-        Button key_5 = findViewById(R.id.key_5);
-        Button key_6 = findViewById(R.id.key_6);
-        Button key_7 = findViewById(R.id.key_7);
-        Button key_8 = findViewById(R.id.key_8);
-        Button key_9 = findViewById(R.id.key_9);
-        Button key_0 = findViewById(R.id.key_0);
-        Button key_divide = findViewById(R.id.key_divide);
-        Button key_clear = findViewById(R.id.key_clear);
-        Button key_multiply = findViewById(R.id.key_multiply);
-        Button key_square_root = findViewById(R.id.key_square_root);
-        Button key_minus = findViewById(R.id.key_minus);
-        Button key_negate = findViewById(R.id.key_negate);
-        Button key_dot = findViewById(R.id.key_dot);
-        Button key_plus = findViewById(R.id.key_plus);
-        Button key_equally = findViewById(R.id.key_equally);
+        Button key1 = findViewById(R.id.key_1);
+        Button key2 = findViewById(R.id.key_2);
+        Button key3 = findViewById(R.id.key_3);
+        Button key4 = findViewById(R.id.key_4);
+        Button key5 = findViewById(R.id.key_5);
+        Button key6 = findViewById(R.id.key_6);
+        Button key7 = findViewById(R.id.key_7);
+        Button key8 = findViewById(R.id.key_8);
+        Button key9 = findViewById(R.id.key_9);
+        Button key0 = findViewById(R.id.key_0);
+        Button keyDivide = findViewById(R.id.key_divide);
+        Button keyClear = findViewById(R.id.key_clear);
+        Button keyMultiply = findViewById(R.id.key_multiply);
+        Button keySquareRoot = findViewById(R.id.key_square_root);
+        Button keyMinus = findViewById(R.id.key_minus);
+        Button keyNegate = findViewById(R.id.key_negate);
+        Button keyDot = findViewById(R.id.key_dot);
+        Button keyPlus = findViewById(R.id.key_plus);
+        Button keyEqually = findViewById(R.id.key_equally);
 
-        keys(key_0, "0");
-        keys(key_1, "1");
-        keys(key_2, "2");
-        keys(key_3, "3");
-        keys(key_4, "4");
-        keys(key_5, "5");
-        keys(key_6, "6");
-        keys(key_7, "7");
-        keys(key_8, "8");
-        keys(key_9, "9");
-        keys(key_divide, "/");
-        keys(key_clear, "clear");
-        keys(key_multiply, "*");
-        keys(key_square_root, "√");
-        keys(key_minus, "-");
-        keys(key_negate, "negate");
-        keys(key_dot, ".");
-        keys(key_plus, "+");
-        keys(key_equally, "=");
+        keys(key0, "0");
+        keys(key1, "1");
+        keys(key2, "2");
+        keys(key3, "3");
+        keys(key4, "4");
+        keys(key5, "5");
+        keys(key6, "6");
+        keys(key7, "7");
+        keys(key8, "8");
+        keys(key9, "9");
+        keys(keyDivide, "/");
+        keys(keyClear, "clear");
+        keys(keyMultiply, "*");
+        keys(keySquareRoot, "√");
+        keys(keyMinus, "-");
+        keys(keyNegate, "negate");
+        keys(keyDot, ".");
+        keys(keyPlus, "+");
+        keys(keyEqually, "=");
 
     }
 
@@ -75,7 +75,9 @@ public class MainActivity extends AppCompatActivity {
                     String temp = display.getText().toString();
                     temp += value;
                     display.setText(temp);
-                } else display.setText(value);
+                } else {
+                    display.setText(value);
+                }
             }
         });
     }
