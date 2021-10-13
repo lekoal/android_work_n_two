6d20043bd134b6547ccd7170c4c6650d6247976f
lekoal <coreglus@gmail.com>
2021-10-13 11:10:51 +0300

    Theme selection layout in production

---
+++ app/src/main/java/com/example/androidworkntwo/ui/ThemeSelectionActivity.java
@@ -0,0 +1,16 @@
+package com.example.androidworkntwo.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.androidworkntwo.R;
+
+public class ThemeSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_theme_selection);
+    }
+}
