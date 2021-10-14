e959b92af8813fddf27725531aa506f3f755d986
lekoal <coreglus@gmail.com>
2021-10-14 06:51:28 +0300

    Theme change abilities in progress

--- app/src/main/java/com/example/androidworkntwo/ui/CalculatorActivity.java
+++ app/src/main/java/com/example/androidworkntwo/ui/CalculatorActivity.java
@@ -4,8 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -37,6 +39,9 @@ public class CalculatorActivity extends AppCompatActivity implements CalculatorV
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        int theme = sp.getInt("THEME", R.style.MyDarkLightTheme);
 
         themeSelection = findViewById(R.id.key_theme_selection);
 
